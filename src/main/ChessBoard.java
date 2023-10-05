707ec7cb0cb7b0c84fc70aaa75b81c3b5ef024df
crwoffinden <crwoffinden@gmail.com>
2023-10-05 08:40:23 -0600

    Create board

---
+++ src/main/ChessBoard.java
@@ -0,0 +1,19 @@
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+public class ChessBoard implements chess.ChessBoard {
+    @Override
+    public void addPiece(ChessPosition position, ChessPiece piece) {
+
+    }
+
+    @Override
+    public ChessPiece getPiece(ChessPosition position) {
+        return null;
+    }
+
+    @Override
+    public void resetBoard() {
+
+    }
+}
